85a95c61069e37c842bdfd33c7e3fb111dfc6df0
BinSa0z <dinhkhoihoang1@gmail.com>
2024-03-15 23:59:56 +0700

    initial commit

--- Web_bansach/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
+++ Web_bansach/WebApplication1/obj/Release/net8.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb4d650e806e47ad618fe802472eec087d31041a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
